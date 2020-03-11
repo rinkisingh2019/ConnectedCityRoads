c53a09bf2327b7a8a3b5e06466024abdd1fb1ac5
rinkisingh2019 <50707282+rinkisingh2019@users.noreply.github.com>
2020-03-11 10:35:34 -0400

    Update IsCityConnectedTest.java

--- src/test/java/com/springboot/city/connected/road/springbootcityconnectedroadservice/IsCityConnectedTest.java
+++ src/test/java/com/springboot/city/connected/road/springbootcityconnectedroadservice/IsCityConnectedTest.java
@@ -4,7 +4,6 @@ package com.springboot.city.connected.road.springbootcityconnectedroadservice;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
 
